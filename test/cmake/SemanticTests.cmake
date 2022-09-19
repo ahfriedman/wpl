793759496df4d8fe9e61196e15e494eb6e3dd65a
ahf <ahftesla@outlook.com>
2022-09-19 09:47:07 -0400

    adding more files

--- test/cmake/SemanticTests.cmake
+++ test/cmake/SemanticTests.cmake
@@ -1,4 +1,5 @@
 # Identify all of the semantic tests
 set(SEMANTIC_TESTS 
   semantic/semantic_tests.cpp
+  semantic/assignment_tests.cpp
 )
---
+++ test/semantic/assignment_tests.cpp
@@ -0,0 +1,160 @@
+#include <catch2/catch_test_macros.hpp>
+#include "antlr4-runtime.h"
+#include "WPLLexer.h"
+#include "WPLParser.h"
+#include "WPLErrorHandler.h"
+#include "SemanticVisitor.h"
+
+#include "test_error_handlers.h"
+
+TEST_CASE("Basic Assignments", "[semantic]")
+{
+  // FIXME: is a <- 2 allowed without a type/previous definition?
+  SECTION("Basic Int Test")
+  {
+    antlr4::ANTLRInputStream input("int a <- 2;");
+    WPLLexer lexer(&input);
+    // lexer.removeErrorListeners();
+    // lexer.addErrorListener(new TestErrorListener());
+    antlr4::CommonTokenStream tokens(&lexer);
+    WPLParser parser(&tokens);
+    parser.removeErrorListeners();
+    parser.addErrorListener(new TestErrorListener());
+
+    WPLParser::CompilationUnitContext *tree = NULL;
+    REQUIRE_NOTHROW(tree = parser.compilationUnit());
+    REQUIRE(tree != NULL);
+    REQUIRE(tree->getText() != "");
+
+    STManager *stmgr = new STManager();
+    SemanticVisitor *sv = new SemanticVisitor(stmgr, new PropertyManager());
+
+    sv->visitCompilationUnit(tree);
+
+    CHECK_FALSE(sv->hasErrors());
+
+    std::optional<Symbol *> opt = stmgr->lookup("a");
+
+    CHECK(opt.has_value());
+    CHECK(opt.value()->type->is(Types::INT));
+  }
+}
+
+TEST_CASE("Assignment: Int Expr", "[semantic]")
+{
+  antlr4::ANTLRInputStream input("int a <- 2 * (2 / 4 + 3 - -2);");
+  WPLLexer lexer(&input);
+  // lexer.removeErrorListeners();
+  // lexer.addErrorListener(new TestErrorListener());
+  antlr4::CommonTokenStream tokens(&lexer);
+  WPLParser parser(&tokens);
+  parser.removeErrorListeners();
+  parser.addErrorListener(new TestErrorListener());
+
+  WPLParser::CompilationUnitContext *tree = NULL;
+  REQUIRE_NOTHROW(tree = parser.compilationUnit());
+  REQUIRE(tree != NULL);
+  REQUIRE(tree->getText() != "");
+
+  STManager *stmgr = new STManager();
+  SemanticVisitor *sv = new SemanticVisitor(stmgr, new PropertyManager());
+
+  sv->visitCompilationUnit(tree);
+
+  CHECK_FALSE(sv->hasErrors());
+
+  std::optional<Symbol *> opt = stmgr->lookup("a");
+
+  CHECK(opt.has_value());
+  CHECK(opt.value()->type->is(Types::INT));
+}
+
+TEST_CASE("Assignment: Bool const", "[semantic]")
+{
+  antlr4::ANTLRInputStream input("boolean a <- false;");
+  WPLLexer lexer(&input);
+  // lexer.removeErrorListeners();
+  // lexer.addErrorListener(new TestErrorListener());
+  antlr4::CommonTokenStream tokens(&lexer);
+  WPLParser parser(&tokens);
+  parser.removeErrorListeners();
+  parser.addErrorListener(new TestErrorListener());
+
+  WPLParser::CompilationUnitContext *tree = NULL;
+  REQUIRE_NOTHROW(tree = parser.compilationUnit());
+  REQUIRE(tree != NULL);
+  REQUIRE(tree->getText() != "");
+
+  STManager *stmgr = new STManager();
+  SemanticVisitor *sv = new SemanticVisitor(stmgr, new PropertyManager());
+
+  sv->visitCompilationUnit(tree);
+
+  CHECK_FALSE(sv->hasErrors());
+
+  std::optional<Symbol *> opt = stmgr->lookup("a");
+
+  CHECK(opt.has_value());
+  CHECK(opt.value()->type->is(Types::BOOL));
+}
+
+TEST_CASE("Assignment: Bool expr", "[semantic]")
+{
+  antlr4::ANTLRInputStream input("boolean a <- (false | true) & ~false;");
+  WPLLexer lexer(&input);
+  // lexer.removeErrorListeners();
+  // lexer.addErrorListener(new TestErrorListener());
+  antlr4::CommonTokenStream tokens(&lexer);
+  WPLParser parser(&tokens);
+  parser.removeErrorListeners();
+  parser.addErrorListener(new TestErrorListener());
+
+  WPLParser::CompilationUnitContext *tree = NULL;
+  REQUIRE_NOTHROW(tree = parser.compilationUnit());
+  REQUIRE(tree != NULL);
+  REQUIRE(tree->getText() != "");
+
+  STManager *stmgr = new STManager();
+  SemanticVisitor *sv = new SemanticVisitor(stmgr, new PropertyManager());
+
+  sv->visitCompilationUnit(tree);
+
+  CHECK_FALSE(sv->hasErrors());
+
+  std::optional<Symbol *> opt = stmgr->lookup("a");
+
+  CHECK(opt.has_value());
+  CHECK(opt.value()->type->is(Types::BOOL));
+}
+
+TEST_CASE("Assignment: String const", "[semantic]")
+{
+  antlr4::ANTLRInputStream input("str a <- \"Hello world!\";");
+  WPLLexer lexer(&input);
+  // lexer.removeErrorListeners();
+  // lexer.addErrorListener(new TestErrorListener());
+  antlr4::CommonTokenStream tokens(&lexer);
+  WPLParser parser(&tokens);
+  parser.removeErrorListeners();
+  parser.addErrorListener(new TestErrorListener());
+
+  WPLParser::CompilationUnitContext *tree = NULL;
+  REQUIRE_NOTHROW(tree = parser.compilationUnit());
+  REQUIRE(tree != NULL);
+  REQUIRE(tree->getText() != "");
+
+  STManager *stmgr = new STManager();
+  SemanticVisitor *sv = new SemanticVisitor(stmgr, new PropertyManager());
+
+  sv->visitCompilationUnit(tree);
+
+  CHECK_FALSE(sv->hasErrors());
+
+  std::optional<Symbol *> opt = stmgr->lookup("a");
+
+  CHECK(opt.has_value());
+  CHECK(opt.value()->type->is(Types::STR));
+}
+
+//FIXME: CAN PARENS BE USED ON STRINGS?
+//FIXME: Add some tests to verify that we don't allow ops to be use incorrectly!
