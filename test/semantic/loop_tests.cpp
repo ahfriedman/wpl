72841321cfa86384350807dd2c51fccf33288da2
ahf <ahftesla@outlook.com>
2022-09-19 10:09:59 -0400

    Added tests

--- test/semantic/loop_tests.cpp
+++ test/semantic/loop_tests.cpp
@@ -45,6 +45,141 @@ TEST_CASE("Undefined Params", "[semantic][loop]")
   // CHECK(opt.value()->type->is(Types::INT));
 }
 
+
+TEST_CASE("Undefined Params 2", "[semantic][loop]")
+{
+  antlr4::ANTLRInputStream input(
+    "int i <- 0;"
+    "while i < 10 do {"
+    " sum <- sum * 2; "
+    " i <- i + 1; "    
+    "}"
+  );
+  WPLLexer lexer(&input);
+  // lexer.removeErrorListeners();
+  // lexer.addErrorListener(new TestErrorListener());
+  antlr4::CommonTokenStream tokens(&lexer);
+  WPLParser parser(&tokens);
+  parser.removeErrorListeners();
+  parser.addErrorListener(new TestErrorListener());
+
+  WPLParser::CompilationUnitContext *tree = NULL;
+  REQUIRE_NOTHROW(tree = parser.compilationUnit());
+  REQUIRE(tree != NULL);
+  REQUIRE(tree->getText() != "");
+
+  STManager *stmgr = new STManager();
+  SemanticVisitor *sv = new SemanticVisitor(stmgr, new PropertyManager());
+
+  sv->visitCompilationUnit(tree);
+
+  //FIXME: CHECK MORE PRECISELY 
+  CHECK(sv->hasErrors());
+
+  // CHECK_FALSE(sv->hasErrors());
+
+  // std::optional<Symbol *> opt = stmgr->lookup("a");
+
+  // CHECK(opt.has_value());
+  // CHECK(opt.value()->type->is(Types::INT));
+}
+
+
+TEST_CASE("Check example", "[semantic][loop]")
+{
+  antlr4::ANTLRInputStream input(
+    "int i <- 0, sum <- 1;"
+    "while i < 10 do {"
+    " sum <- sum * 2; "
+    " i <- i + 1; "    
+    "}"
+  );
+  WPLLexer lexer(&input);
+  // lexer.removeErrorListeners();
+  // lexer.addErrorListener(new TestErrorListener());
+  antlr4::CommonTokenStream tokens(&lexer);
+  WPLParser parser(&tokens);
+  parser.removeErrorListeners();
+  parser.addErrorListener(new TestErrorListener());
+
+  WPLParser::CompilationUnitContext *tree = NULL;
+  REQUIRE_NOTHROW(tree = parser.compilationUnit());
+  REQUIRE(tree != NULL);
+  REQUIRE(tree->getText() != "");
+
+  STManager *stmgr = new STManager();
+  SemanticVisitor *sv = new SemanticVisitor(stmgr, new PropertyManager());
+
+  sv->visitCompilationUnit(tree);
+
+  //FIXME: CHECK MORE PRECISELY 
+  CHECK_FALSE(sv->hasErrors());
+
+  // CHECK_FALSE(sv->hasErrors());
+
+  // std::optional<Symbol *> opt = stmgr->lookup("a");
+
+  // CHECK(opt.has_value());
+  // CHECK(opt.value()->type->is(Types::INT));
+}
+
+TEST_CASE("Check example - adv", "[semantic][loop]")
+{
+  antlr4::ANTLRInputStream input(
+    "int i <- 0, sum <- 1;"
+    "while i < 10 do {"
+    " sum <- sum * 2; "
+    " i <- i + 1; "  
+    " str s <- \"scope\";"  
+    "}"
+  );
+  WPLLexer lexer(&input);
+  // lexer.removeErrorListeners();
+  // lexer.addErrorListener(new TestErrorListener());
+  antlr4::CommonTokenStream tokens(&lexer);
+  WPLParser parser(&tokens);
+  parser.removeErrorListeners();
+  parser.addErrorListener(new TestErrorListener());
+
+  WPLParser::CompilationUnitContext *tree = NULL;
+  REQUIRE_NOTHROW(tree = parser.compilationUnit());
+  REQUIRE(tree != NULL);
+  REQUIRE(tree->getText() != "");
+
+  STManager *stmgr = new STManager();
+  SemanticVisitor *sv = new SemanticVisitor(stmgr, new PropertyManager());
+
+  sv->visitCompilationUnit(tree);
+
+  //FIXME: CHECK MORE PRECISELY 
+  CHECK_FALSE(sv->hasErrors());
+
+  // CHECK_FALSE(sv->hasErrors());
+
+  {
+    std::optional<Symbol *> opt = stmgr->lookup("a");
+    CHECK_FALSE(opt.has_value());
+  }
+
+  {
+    std::optional<Symbol *> opt = stmgr->lookup("i");
+    CHECK(opt.has_value());
+    CHECK(opt.value()->type->is(Types::INT));
+  }
+
+
+  {
+    std::optional<Symbol *> opt = stmgr->lookup("sum");
+    CHECK(opt.has_value());
+    CHECK(opt.value()->type->is(Types::INT));
+  }
+
+  {
+    std::optional<Symbol *> opt = stmgr->lookup("s");
+    CHECK_FALSE(opt.has_value());
+  }
+}
+
 TEST_CASE("Sit & Spin", "[semantic][loop]")
 {
   antlr4::ANTLRInputStream input(
