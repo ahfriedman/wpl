ee66f065efe0ab2d11c7636fc7b0023b4aadc326
ahf <ahftesla@outlook.com>
2022-10-10 14:26:57 -0400

    More tests!

--- test/semantic/program_tests.cpp
+++ test/semantic/program_tests.cpp
@@ -282,6 +282,31 @@ TEST_CASE("programs/doubleArg2 - Prevent Argument reuse in extern", "[semantic]"
     REQUIRE(sv->hasErrors(0));
 }
 
+TEST_CASE("programs/doubleArg3 - Prevent Argument reuse in func and that we don't crash", "[semantic]")
+{
+    std::fstream *inStream = new std::fstream("/home/shared/programs/doubleArg3.wpl");
+    antlr4::ANTLRInputStream * input = new antlr4::ANTLRInputStream(*inStream);
+
+    WPLLexer lexer(input);
+    antlr4::CommonTokenStream tokens(&lexer);
+    WPLParser parser(&tokens);
+    parser.removeErrorListeners();
+    WPLParser::CompilationUnitContext *tree = NULL;
+    REQUIRE_NOTHROW(tree = parser.compilationUnit());
+    REQUIRE(tree != NULL);
+    STManager *stm = new STManager();
+    PropertyManager *pm = new PropertyManager();
+    SemanticVisitor *sv = new SemanticVisitor(stm, pm);
+    sv->visitCompilationUnit(tree);
+
+
+    // if(sv->hasErrors(0))
+    // {
+    //     CHECK("foo" == sv->getErrors());
+    // }
+    REQUIRE(sv->hasErrors(0));
+}
+
 /*********************************
  * C-Level Example tests
  *********************************/
