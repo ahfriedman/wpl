c5ef3307d40898c0db96f27be299b7bc038299dc
ahf <ahftesla@outlook.com>
2022-11-27 09:57:15 -0500

    Added new test

--- test/codegen/codegen_tests.cpp
+++ test/codegen/codegen_tests.cpp
@@ -1239,6 +1239,34 @@ TEST_CASE("programs/enumAssign - Same a  Enum 2 but with assignmens outside of d
     REQUIRE(llvmIrToSHA256(cv->getModule()) == "5fb322db3a1c17135df54896d03d1312116a66754a7504660052950f6390e0df");
 }
 
+TEST_CASE("programs/enumAssign2 - Returning lambdas, functions, and enums", "[codegen][enum][lambda]")
+{
+    std::fstream *inStream = new std::fstream("/home/shared/programs/enumAssign2.wpl");
+    antlr4::ANTLRInputStream * input = new antlr4::ANTLRInputStream(*inStream);
+
+    WPLLexer lexer(input);
+    antlr4::CommonTokenStream tokens(&lexer);
+    WPLParser parser(&tokens);
+    parser.removeErrorListeners();
+    WPLParser::CompilationUnitContext *tree = NULL;
+    REQUIRE_NOTHROW(tree = parser.compilationUnit());
+    REQUIRE(tree != NULL);
+    STManager *stm = new STManager();
+    PropertyManager *pm = new PropertyManager();
+    SemanticVisitor *sv = new SemanticVisitor(stm, pm, 0);
+    sv->visitCompilationUnit(tree);
+
+
+    REQUIRE_FALSE(sv->hasErrors(0));
+
+    CodegenVisitor *cv = new CodegenVisitor(pm, "WPLC.ll", 0);
+    cv->visitCompilationUnit(tree);
+
+    REQUIRE_FALSE(cv->hasErrors(0));
+
+    REQUIRE(llvmIrToSHA256(cv->getModule()) == "9153d63cc359041fb8e328cd523af59085257fdac3bfc74c85d8efca5caddf60");
+}
+
 TEST_CASE("programs/enum3", "[codegen][enum]")
 {
     std::fstream *inStream = new std::fstream("/home/shared/programs/enum3.wpl");
